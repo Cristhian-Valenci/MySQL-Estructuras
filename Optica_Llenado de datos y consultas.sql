957e00c4dad9712ea5cf80f776e547e2c10c7f98
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-09-13 14:13:20 +0200

    Correccion de ejercicio Nivel 1 de estructuras MySQL

--- Optica_Llenado de datos y consultas.sql
+++ Optica_Llenado de datos y consultas.sql
@@ -1,6 +1,6 @@
-INSERT INTO clientes (nombre, apellido, direccion, email, fecha_registro, recomendacion)
+INSERT INTO clientes (nombre, apellido, direccion, email, fecha_registro, id_cliente_que_recomendo_a_este)
 VALUES ( 'juan', 'perez', 'corsega 536', 'juanpe@gmail.com', '2025-04-10', 2);
-INSERT INTO clientes (nombre, apellido, direccion, email, fecha_registro, recomendacion)
+INSERT INTO clientes (nombre, apellido, direccion, email, fecha_registro, id_cliente_que_recomendo_a_este)
 VALUES ( 'carla', 'lopez', 'paris 80', 'carlita@gmail.com', '2025-03-22', 1);
 
 INSERT INTO empleado (nombre, apellido)
@@ -8,10 +8,16 @@ VALUES ('cristhian', 'valenci');
 INSERT INTO empleado(nombre, apellido)
 VALUES ('hernan', 'rodriguez');
 
-INSERT INTO gafas (marca, graduacion, tipo_montura, color_montura, color_cristal, precio, id_proveedor)
-VALUES ('rayban', '80', 'metal', 'negro', 'gris', '89', 1);
-INSERT INTO gafas (marca, graduacion, tipo_montura, color_montura, color_cristal, precio, id_proveedor)
-VALUES ('adidas', '60', 'plastico', 'verde', 'negro', '99', 2);
+INSERT INTO Marca_de_gafas (nombre, id_proveedor)
+VALUES ('rayban', 2);
+INSERT INTO Marca_de_gafas (nombre, id_proveedor)
+VALUES ('merrel', 1);
+
+
+INSERT INTO gafas (graduacion, tipo_montura, color_montura, color_cristal, precio, id_proveedor, id_marca)
+VALUES (2.50, 'flotante', 'negro', 'gris', 99.99, 1, 2);
+INSERT INTO gafas (graduacion, tipo_montura, color_montura, color_cristal, precio, id_proveedor, id_marca)
+VALUES (1.25, 'metalica', 'verde', 'negro', 120.00, 2, 1);
 
 INSERT INTO proveedor (nombre, apellido, telefono, direccion, nif)
 VALUES ('lorenzo', 'vario', '6000909009', 'napols 89', '4545454545l');
@@ -30,8 +36,6 @@ WHERE id_clientes = 1;
 SELECT * FROM gafas
 JOIN ventas ON ventas.id_gafas = gafas.id_gafas
 JOIN empleado ON empleado.id_empleado = ventas.id_empleado
-
-
 WHERE ventas.id_empleado = 1
 AND ventas.fecha LIKE '2023%';
 
@@ -39,4 +43,5 @@ SELECT distinct * FROM proveedor
 JOIN gafas ON gafas.id_proveedor = proveedor.id_proveedor
 JOIN ventas ON ventas.id_gafas = gafas.id_gafas
 WHERE ventas.id_gafas IS NOT NULL;
+
 
--- Pizzeria_llenadoDatos_requisitos.sql
+++ Pizzeria_llenadoDatos_requisitos.sql
@@ -8,10 +8,10 @@ VALUES ('chiara', 'vario', '676767676m', '454545454', 'repartidor', '1');
 INSERT INTO empleado(nombre, apellido, nif, telefono, tipo, id_tienda)
 VALUES ('carlos', 'gomez', '232323232f', '56565656', 'cocinero', '1');
 
-INSERT INTO pedido (fecha_hora_recibido, modo_entrega, id_producto, precio, id_cliente, id_tienda)
-VALUES ( '2025-04-23 22:45', 'domicilio', '1', '35', '2', '1');
-INSERT INTO pedido (fecha_hora_recibido, modo_entrega, id_producto, precio, id_cliente, id_tienda)
-VALUES ( '2025-04-25 12:30', 'recogida', '2', '45', '1', '2');
+INSERT INTO pedido (fecha_hora_recibido, modo_entrega, precio, id_cliente, id_tienda)
+VALUES ( '2025-04-23 22:45', 'domicilio', '12.99', '2', '1');
+INSERT INTO pedido (fecha_hora_recibido, modo_entrega, precio, id_cliente, id_tienda)
+VALUES ( '2025-04-25 12:30', 'recogida', '10.00', '1', '2');
 
 INSERT INTO pedido_a_domicilio ( fecha_hora_entrega, id_empleado, id_pedido)
 VALUES ('2025-04-23 22:55', '1', '1');
@@ -24,23 +24,23 @@ INSERT INTO categoria_pizza (nombre)
 VALUES ('kentuky');
 
 INSERT INTO producto (nombre, descripcion, imagen, tipo, precio, id_pedido, id_categoria_pizza)
-VALUES ('margarita', 'salsa de tomate y muzzarella', 'IMG 3450', 'pizza','12', '1', '1');
+VALUES ('margarita', 'salsa de tomate y muzzarella', 'IMG 3450', 'pizza', '12.99', '1', '1');
 INSERT INTO producto (nombre, descripcion, imagen, tipo, precio, id_pedido, id_categoria_pizza)
-VALUES ('cerveza', 'estrella galicia', 'IMG 2345', 'bebida','3', '1', NULL );
+VALUES ('cerveza', 'estrella galicia', 'IMG 2345', 'bebida', '3.00', '1', NULL );
 
 INSERT INTO tienda (direccion, codigo_postal, localidad, provincia)
 VALUES ('via laietana 2', '08005', 'barcelona', 'barcelona');
 INSERT INTO tienda (direccion, codigo_postal, localidad, provincia)
 VALUES ('marina 23', '08007', 'barcelona', 'barcelona');
 
-INSERT INTO cantidad_productos( id_pedido, id_producto, cantidad)
+INSERT INTO cantidad_de_productos( pedido_id_pedido, producto_id_producto, cantidad_de_productoscol)
 VALUES (1, 2, 3);
-INSERT INTO cantidad_productos( id_pedido, id_producto, cantidad)
+INSERT INTO cantidad_de_productos( pedido_id_pedido, producto_id_producto, cantidad_de_productoscol)
 VALUES (2, 2, 4);
 
-SELECT SUM(cantidad_productos.cantidad) FROM cantidad_productos
-JOIN pedido ON pedido.id_producto = cantidad_productos.id_producto
-JOIN producto ON producto.id_producto = pedido.id_producto
+SELECT COUNT(cantidad_de_productos.cantidad_de_productoscol) FROM cantidad_de_productos
+JOIN  producto ON producto.id_producto = cantidad_de_productos.producto_id_producto
+JOIN pedido ON pedido.id_pedido = producto.id_pedido
 JOIN tienda ON tienda.id_tienda = pedido.id_tienda
 WHERE producto.tipo LIKE 'bebida'
 AND tienda.localidad LIKE 'barcelona';
